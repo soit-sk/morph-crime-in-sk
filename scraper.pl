b35010db7b31e30f128b5223a44766ecd750a845
Lubomir Rintel <lkundrak@v3.sk>
2015-10-10 15:55:46 +0200

    Unbreak

--- scraper.pl
+++ scraper.pl
@@ -13,22 +13,13 @@ use XML::Simple;
 use Database::DumpTruck;
 
 binmode STDOUT, ':utf8';
-# http://www.minv.sk/?statistika_kriminality_v_slovenskej_republike_za_rok_2013
-my $root = 'http://www.minv.sk/?kriminalita_2014_xml';
 my $dt = new Database::DumpTruck ({ dbname => 'data.sqlite', table => 'data' });
 
-# Array utilities
-sub array_add
-{
-	my ($a1, $a2) = @_;
-	map { $a1->[$_] + ($a2->[$_] || 0) } (0..$#$a1);
-}
+# Uniquely identifies a category
+my @id = ('Department Code', 'Department', 'Category', 'Sub-category', 'Sub-category 2', 'Sub-category 3');
 
-sub array_sub
-{
-	my ($a1, $a2) = @_;
-	map { $a1->[$_] - ($a2->[$_] || 0) } (0..$#$a1);
-}
+# Uniquely identifies an database entry (usable in an unique index)
+my @keys = ('Year', 'Month', 'Month_Year', @id);
 
 # Return only sanitized content, no styles
 sub sane_row
@@ -50,7 +41,7 @@ sub grok_sheet
 	my $sheet = shift;
 
 	my $dept_code = $sheet->{'ss:Name'};
-	return if $dept_code eq 'SR'; # Aggregation
+	return {} if $dept_code eq 'SR'; # Aggregation
 	my @rows = map { sane_row $_ } @{$sheet->{Table}{Row}};
 
 	# Assertion check
@@ -60,7 +51,7 @@ sub grok_sheet
 	# Sanitize date span
 	shift (@rows)->[0] =~ /ZA OBDOBIE\s+(\d+)\.(\d+)\.\s+-\s+(\d+)\.(\d+)\.(\d+)/
 		or die 'A wave hit the ship';
-	my ($month, $year) = (sprintf ('%02d', $2), $5);
+	my ($month, $year) = (sprintf ('%02d', $4), $5);
 
 	# Department
 	shift (@rows)->[0] =~ /^(.daje za .*)/
@@ -76,7 +67,7 @@ sub grok_sheet
 	splice @headings, 2, 1; # meaningless %
 
 	# Read the actual body
-	my @result;
+	my %result;
 	my @cat_path = (undef, undef, undef, undef);
 	while (my $row = shift @rows) {
 		my ($category, @data) = @$row;
@@ -93,11 +84,11 @@ sub grok_sheet
 			# Sanitize the name
 			$category =~ s/\s+/ /g;
 			$category =~ s/(.)(.*)/$1\L$2/;
-			@cat_path[0..$#cat_path] = ($category, undef, undef, undef);
+			@cat_path[0..$#cat_path] = ($category, '', '', '');
 		} elsif ($category !~ /^\s+-/) {
-			@cat_path[1..$#cat_path] = ($category, undef, undef);
+			@cat_path[1..$#cat_path] = ($category, '', '');
 		} elsif ($category =~ /^\s{2,4}-\s*(.+)/) {
-			@cat_path[2..$#cat_path] = ($1, undef);
+			@cat_path[2..$#cat_path] = ($1, '');
 		} elsif ($category =~ /^\s{5,8}-\s*(.+)/) {
 			@cat_path[3..$#cat_path] = ($1);
 		} else {
@@ -108,45 +99,24 @@ sub grok_sheet
 		@data = map { $_ || 0 } @data;
 
 		# Construct the real row
-		my @res = map { $_ ? $_ : () } @cat_path;
-		$res[3] = $res[3] || undef;
+		#my @res = map { defined $_ ? $_ : () } @cat_path;
+		#$res[3] = $res[3] || undef;
 
-		push @result, [\@res, \@data];
+		my %row;
+		@row{@keys, @headings} = ($year, $month, $year.$month, $dept_code, $dept_human, @cat_path, @data);
+		my $key = join "|", map { $row{$_} } @id;
+		$result{$key} = \%row;
 	}
 
-	# Fix up aggregations
-	foreach my $level (1..3) {
-		my @agg;
-		foreach my $row (reverse @result) {
-			my ($cat_path, $data) = @$row;
-
-			if ($cat_path->[$level + 1]) {
-				next;
-			} elsif ($cat_path->[$level]) {
-				@agg = array_add ($data, \@agg);
-			} elsif ($cat_path->[$level - 1]) {
-				next unless @agg;
-				$cat_path->[$level] = '(ostatne)';
-				@$data = array_sub ($data, \@agg);
-				@agg = ();
-			}
-		}
-	}
-
-	# Enrich with common data
-	unshift @headings, 'Year', 'Month', 'Month/Year', 'Department Code', 'Department',
-		'Category', 'Sub-category', 'Sub-category 2', 'Sub-category 3';
-	@result = map { [ $year, $month, $year.$month, $dept_code, $dept_human, @{$_->[0]}, @{$_->[1]} ] }
-		@result;
-
-	return (\@headings, \@result);
+	return \%result;
 }
 
 # Format document
 sub grok_xml
 {
 	# Absolutize
-	my $uri = new URI (shift)->abs ($root);
+	my $uri = shift;
+	my %month;
 
 	use URI::Escape;
 	my $base = uri_unescape (basename($uri));
@@ -154,29 +124,72 @@ sub grok_xml
 
 	my @rows;
 	my $xml = XMLin (get($uri));
-	foreach my $sheet (@{$xml->{Worksheet}}) {
-		next unless my ($headers, $data) = grok_sheet ($sheet);
-		s/[^a-zA-Z_0-9]/_/g foreach @$headers;
-		foreach my $row (@$data) {
-			push @rows, {};
-			@{$rows[$#rows]}{@$headers} = @$row;
+	%month = (%month, %{grok_sheet ($_)}) foreach @{$xml->{Worksheet}};
+
+	return \%month;
+}
+
+sub month_sub
+{
+	my $a = shift;
+	my $b = shift;
+
+	return $a unless $b;
+
+	foreach my $id (keys %$a) {
+		foreach my $key (keys %{$a->{$id}}) {
+			next if grep { $_ eq $key } @keys;
+			$a->{$id}{$key} -= $b->{$id}{$key};
 		}
 	}
 
-	$dt->insert (\@rows);
+	return $a;
 }
+
+sub process_year
+{
+	my $uri = shift;
+
+	my $previous_month;
+	my $parser = new HTML::Parser (api_version => 3);
+	$parser->handler (start => sub {
+		my $tag = shift;
+		my $attr = shift;
+
+		return unless $tag eq 'a';
+		return unless exists $attr->{href};
+		return unless $attr->{href} =~ m{/swift_data/source/policia/statistiky.*.xml};
+		my $this_month = grok_xml (new URI ($attr->{href})->abs ($uri)) or return;
+
+		$this_month = month_sub ($this_month, $previous_month);
+		$previous_month = $this_month;
+
+		$dt->upsert ([values %$this_month]);
+	}, 'tag, attr');
+
+	$parser->parse (get $uri or die "I get it, but I don't get it. -- Alice Cooper");
+	$parser->eof;
+
+}
+
+my $root = 'http://www.minv.sk/?statistika-kriminality-v-slovenskej-republike-xml';
 
 # Roll!
 my $parser = new HTML::Parser (api_version => 3);
+$parser->unbroken_text (1);
 $parser->handler (start => sub {
 	my $tag = shift;
 	my $attr = shift;
 
 	return unless $tag eq 'a';
 	return unless exists $attr->{href};
-	return unless $attr->{href} =~ m{/swift_data/source/policia/statistiky/.*.xml};
-	grok_xml ($attr->{href});
+	return unless $attr->{href} =~ m{/.*za.rok.(\d\d\d\d)$};
+
+	process_year (new URI ($attr->{href})->abs ($root));
 }, 'tag, attr');
 
-$parser->parse (get $root or die "I get it, but I don't get it. -- Alice Cooper");
+$parser->parse (get $root or die);
 $parser->eof;
+
+# Ensure there's indices when we know there's a scheme
+$dt->create_index (\@keys, undef, 1, 1);
